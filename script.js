45d33254598b911dbf1d71ed73d8c2a9207408b7
Jejsksjfh <145230166+Jejsksjfh@users.noreply.github.com>
2023-09-21 16:21:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,35 @@ document.querySelectorAll("nav ul li a").forEach(function(link) {
         event.preventDefault();
         var target = document.querySelector(this.getAttribute("href"));
         window.open(this.getAttribute("href"), '_blank'); // Открываем раздел в новом окне
+
+// Функция для очистки полей в форме обратной связи
+function clearFeedbackForm() {
+    document.getElementById("feedback").reset();
+}
+
+// Функция для очистки всех полей формы обратной связи
+function clearFeedbackForm() {
+    var nameInput = document.getElementById("имя");
+    var emailInput = document.getElementById("email");
+    var messageInput = document.getElementById("сообщение");
+
+    nameInput.value = "";
+    emailInput.value = "";
+    messageInput.value = "";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('feedback');
+
+    form.addEventListener('focusin', function(event) {
+        if (event.target.value === event.target.defaultValue) {
+            event.target.value = '';
+        }
+    });
+
+    form.addEventListener('focusout', function(event) {
+        if (event.target.value === '') {
+            event.target.value = event.target.defaultValue;
+        }
     });
 });
-
